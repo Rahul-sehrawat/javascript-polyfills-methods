286399ab2937bc5b580d1ce8c9bb17e23cd843cb
Rahul sehrawat <rahulsehrawat1205@gmail.com>
2024-08-07 15:18:46 +0530

    reduce polyfill added

---
+++ reduce polyfill.js
@@ -0,0 +1,19 @@
+// polyfill for reduce()
+
+Array.prototype.myReduce = function(cb,initialValue){
+    var accumulator = initialValue;
+
+    for (let i = 0; i < this.length; i++){
+        accumulator = accumulator ? cb(accumulator,this[i],i,this): this[i];
+    }
+    return accumulator
+}
+
+
+const nums = [1,2,3,5]
+
+const sum = nums.myReduce((acc,curr,i,arr)=>{
+    return acc + curr
+ },0)
+
+console.log(sum)
